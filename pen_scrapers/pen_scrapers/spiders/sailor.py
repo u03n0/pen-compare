e5b1e14929aa6eeab5397f07bfa234c0b1043ff4
u03n0 <u03n0@proton.me>
2025-02-01 12:42:53 +0100

    using FountainPenItem for sailor

--- pen_scrapers/pen_scrapers/spiders/sailor.py
+++ pen_scrapers/pen_scrapers/spiders/sailor.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from pen_scrapers.items import FountainPenItem
 
 class SailorSpider(scrapy.Spider):
 
@@ -21,22 +21,17 @@ class SailorSpider(scrapy.Spider):
 
     def parse_product(self, response):
         """ Get more information from each product """
-
-        name = response.css("h2.ProductEyecatch__productNameMain::text").get()
-        color_ways = response.css("label.ProductEyecatch__colorLabel::attr(style)").re(r"background-color:\s*(#[0-9a-fA-F]{6})")
-        color_names = response.css("label.ProductEyecatch__colorLabel::attr(data-color)").extract()
-        description = response.css("div.Common__inputArticle::text").extract()
+        pen = FountainPenItem()
+        pen['brand'] = 'sailor'
+        pen['name'] = response.css("h2.ProductEyecatch__productNameMain::text").get()
+        pen['colors'] = response.css("label.ProductEyecatch__colorLabel::attr(style)").re(r"background-color:\s*(#[0-9a-fA-F]{6})")
+        pen['color_names'] = response.css("label.ProductEyecatch__colorLabel::attr(data-color)").extract()
+        pen['description'] = response.css("div.Common__inputArticle::text").extract()
         detail_names = response.css("div.ProductInfoTab__tabTarget dt::text").extract()
-
+        
         details = {} 
         for _name in detail_names:
             detail_value = response.css(f"dt.ProductInfoTab__itemHead:contains('{_name}') + dd::text").getall()
             details[_name] = [val.strip() for val in detail_value]
-
-        yield {
-                "name": name,
-                "color_ways": color_ways,
-                "color_names": color_names,
-                "description": description,
-                "details": details
-                }
+        pen['details'] = details
+        yield pen
